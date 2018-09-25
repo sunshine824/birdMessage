5bef3a0c5d5e2f6512107572eeca066d40a5d32a
sunshine824 <790168710@qq.com>
2018-09-25 17:29:23 +0800

    WEB服务器搭建

--- Web/ITalker/src/main/java/net/qiujuer/web/italker/push/Application.java
+++ Web/ITalker/src/main/java/net/qiujuer/web/italker/push/Application.java
@@ -1,4 +1,23 @@
 package net.qiujuer.web.italker.push;
 
-public class Application {
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.qiujuer.web.italker.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
+/**
+ * @author chenxin
+ */
+public class Application extends ResourceConfig {
+    public Application() {
+        //注册逻辑处理的包名
+        packages(AccountService.class.getPackage().getName());
+
+        //注册Json解析器
+        register(JacksonJsonProvider.class);
+
+        //注册日志打印输出
+        register(Logger.class);
+    }
 }
